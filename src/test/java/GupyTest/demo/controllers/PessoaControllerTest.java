f45f4ad43385ffc6a1ad3384f578a37a893e9894
Bruno Guimarães <brunoguima13@gmail.com>
2023-02-10 16:43:36 -0300

    Adding more Unitary Test

--- src/test/java/GupyTest/demo/controllers/PessoaControllerTest.java
+++ src/test/java/GupyTest/demo/controllers/PessoaControllerTest.java
@@ -3,12 +3,14 @@ package GupyTest.demo.controllers;
 import GupyTest.demo.models.entities.Endereco;
 import GupyTest.demo.models.entities.Pessoa;
 import GupyTest.demo.models.entities.repositories.PessoaRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +18,42 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class PessoaControllerTest {
     @Autowired
-    PessoaRepository repository;
+    PessoaRepository repositoryTest;
+
+    @AfterEach
+    void tearDown() {
+        repositoryTest.deleteAll();
+    }
+
     @Test
-    void ItShouldSetLastEnderecoIfPrincipalValueToTrue() {
+    void itShouldTestIfBothEnderecosAreEquals(){
+        //given
+        Endereco endereco1 = new Endereco(
+                "Rua Teste",
+                "cep teste",
+                123,
+                "Cidade teste",
+                false
+        );
+        Endereco endereco2 = new Endereco(
+                "Rua Teste",
+                "cep teste",
+                321,
+                "Cidade teste",
+                false
+        );
+        List<Endereco> enderecos = new ArrayList<>();
+        enderecos.add(endereco1);
+        enderecos.add(endereco2);
+        //when
+        Pessoa pessoa = new Pessoa("Bruno Guimaraes", "13/11/2000", enderecos);
+        repositoryTest.save(pessoa);
+        //then
+        assertArrayEquals(repositoryTest.findById(pessoa.getId()).get().getEndereco().toArray(), enderecos.toArray());
+    }
+
+    @Test
+    void itShouldSetLastEnderecoIfPrincipalValueToTrue() {
         //given
         Endereco endereco = new Endereco(
                 "Rua Teste",
@@ -49,7 +84,7 @@ class PessoaControllerTest {
         pessoa.getEndereco().stream().forEach(data -> data.setIfPrincipal(false));
         pessoa.getEndereco().get(pessoa.getEndereco().size()-1).setIfPrincipal(true);
         //then
-        repository.save(pessoa);
-        assertTrue(pessoa.getEndereco().get(pessoa.getEndereco().size()-1).isIfPrincipal());
+        repositoryTest.save(pessoa);
+        assertTrue(repositoryTest.findById(pessoa.getId()).get().getEndereco().get(pessoa.getEndereco().size()-1).isIfPrincipal());
     }
 }
